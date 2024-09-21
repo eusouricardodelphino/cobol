b010fe850b48eb27afb48d3b4709f7aa29193543
Ricardo Delphino <radelphino@gmail.com>
2024-09-20 23:34:39 -0300

    first commit

---
+++ PROGCOB07.cob
@@ -0,0 +1,28 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROGCOB07.
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SPECIAL-NAMES.
+          DECIMAL-POINT IS COMMA.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       77 WRK-NOTA1 PIC 9(2)V9 VALUE ZEROS.
+       77 WRK-NOTA2 PIC 9(2)V9 VALUE ZEROS.
+       77 WRK-MEDIA PIC 9(2)V99 VALUE ZEROS.
+       PROCEDURE DIVISION.
+           ACCEPT WRK-NOTA1.
+           ACCEPT WRK-NOTA2.
+           COMPUTE WRK-MEDIA = (WRK-NOTA1 + WRK-NOTA2) / 2.
+           
+           IF WRK-MEDIA < 2
+             DISPLAY "REPROVADO"
+           ELSE
+             IF WRK-MEDIA < 6
+               DISPLAY "RECUPERAÇÃO"
+             ELSE
+               DISPLAY 'APROVADO'
+             END-IF
+           END-IF.
+
+           DISPLAY 'MEDIA: ' WRK-MEDIA.
+           STOP RUN.
